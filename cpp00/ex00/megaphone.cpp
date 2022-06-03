3f93b8ece95e9497a8d62874fdad168b8a25f555
Julen Smith <jsmith@MacBook-Pro-de-Julen.local>
2022-06-03 16:55:09 +0200

    to upper pocho

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,13 +6,30 @@
 /*   By: jsmith <jsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 16:33:17 by jsmith            #+#    #+#             */
-/*   Updated: 2022/06/03 16:34:17 by jsmith           ###   ########.fr       */
+/*   Updated: 2022/06/03 16:53:05 by jsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello World!";
+int main(int argc, char *argv[]) {
+    
+	int i;
+	int u;
+
+	i = 1;
+	while(argv[i])
+	{	
+		u = 0;
+		while (argv[i][u])
+		{
+			std::cout << (char)toupper(argv[i][u]);
+			u++;
+		}
+		//std::cout << std::endl; ???
+		i++;	
+	}
     return 0;
 }
+

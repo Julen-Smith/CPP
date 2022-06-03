ec63ee10418a7729f5bbc49bbf93cc80ac570b63
Julen Smith <jsmith@MacBook-Pro-de-Julen.local>
2022-06-03 17:09:09 +0200

    fonbuk

---
+++ cpp00/ex01/contact.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class contact
+{
+	private: 
+		std::string first_name;
+	 	std::string last_name;
+		std::string nickname;
+	 	int phone_number;
+	 	std::string darkest_secret;
+
+	
+
+
+
+
+
+
+
+
+};

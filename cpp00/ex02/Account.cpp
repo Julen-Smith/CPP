1c86d6b34892813b3632f92486954b77691c3f2e
ordorika-benat <bgarrido42@up2u.localhost>
2022-06-04 22:09:35 +0000

    Initial commit for ex02

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Account.hpp"
+
+	Account::Account(int initial_deposit) {}
+	Account::~Account() {}
+
+	static int Account::getNbAccounts(void) {
+		return (1);
+	}
+	static int Account::getTotalAmount(void) {
+		return (1)
+}
+static int	Account::getNbDeposits( void ){}
+static int	Account::getNbWithdrawals( void ){}
+static void	Account::displayAccountsInfos( void ){}
+void	Account::makeDeposit( int deposit ){}
+bool	Account::makeWithdrawal( int withdrawal ){}
+int		Account::checkAmount( void ) const{}
+void	Account::displayStatus( void ) const{}

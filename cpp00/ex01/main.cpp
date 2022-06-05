6b9bdb372f37cd86b14279dbbb65dbe53e17307e
ordorika-benat <bgarrido42@up2u.localhost>
2022-06-05 20:35:17 +0000

    EX01 finished correctly

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -14,7 +14,7 @@
 
 
 
-int print_menu(void)
+void print_menu(void)
 {
 	std::cout << "Choose your action (select number:" << std::endl;
 	std::cout << "	1. ADD" << std::endl;
@@ -23,12 +23,11 @@ int print_menu(void)
 }
 
 
-int main(int argc, char *argv[])
+int main(void)
 {
 	char response[MAX_NAME_LEN]; 
 	phone_book pb;
 	int contact_index;
-	int contact_position;
 	
 	contact_index = 0;
 	while (true)
@@ -40,15 +39,15 @@ int main(int argc, char *argv[])
 		print_menu();
 		std::cin.getline(response,MAX_NAME_LEN);
 	
-		if (!strcmp(response,"1"))
+		if (!strcmp(response,"ADD"))
 			option = 1;
-		else if (!strcmp(response,"2"))
+		else if (!strcmp(response,"SEARCH"))
 			option = 2;
 			
 		switch(option)
 		{
 			default : 
-				exit(0);	
+				exit(0);
 			case 1:
 				pb.add(pb.create_contact(),contact_index);
 				contact_index++;
@@ -57,57 +56,5 @@ int main(int argc, char *argv[])
 				pb.search(contact_index);
 				break;
 		}
-			
-		
-		//Introducir al objeto.
-	//	pb.contacts[0].set_first_name((std::string)response); 
-	//	std::cout << pb.contacts[0].get_first_name() << std::endl;
-	
-
-		
-		/*
-		switch (option) {
-			case 1:
-
-			break;
-			case 2:
-
-			
-
-		
-		
-		switch (opt) {
-			case 1:
-				std::cout << "Insert name: " << std::endl;
-				std::cin >> firstName;
-				ct.set_first_name(firstName);
-				std::cout << "Insert last name: " << std::endl;
-				std::cin >> lastName;
-				ct.set_last_name(lastName);
-				std::cout << "Insert nickname: " << std::endl;
-				std::cin >> nickname;
-				ct.set_nickname(nickname);
-				std::cout << "Insert phone number: " << std::endl;
-				std::cin >> phoneNumber;
-				ct.set_phone_number(phoneNumber);
-				std::cout << "Insert darkest secret: " << std::endl;
-				std::cin >> darkestSecret;
-				ct.set_darkest_secret(darkestSecret);
-				ct.set_contact_index(contact_index);
-				ct.set_contact_position(contact_position);
-				pb.add(ct, contact_position);
-				break;
-			case 2:
-				pb.search();
-				break;
-			case 3:
-				exit(1);
-			default:
-				std::cout << "Wrong mf\n";
-				exit(1);
-		}
-		*/
-	
-	
 	}
 }

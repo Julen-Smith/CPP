434ead5e049c50109a8a8a5260278ecc1cbbe647
Julen Smith <jsmith@MacBook-Pro-de-Julen.local>
2022-06-07 23:32:22 +0200

    working ma o meno

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#define REPLACE_PATH "FILENAME.replace"
+
+/*
+	Se hace append constante del fichero antiguo en el nuevo
+	Se mete un salto de linea extra.
+	Solo cambia la primera coincidencia-
+*/
+
+std::string replaceSingleCoincidence(std::string line,std::string str1, std::string str2) 
+{
+	int pos = line.find(str1, 0);
+	if(pos > 0)
+	{
+		line.erase(pos,str1.length());
+		line.insert(pos,str2);
+		pos = line.find(str1, 0);
+		if (pos > 0)
+		{
+			while(pos > 0)
+			{	
+				pos = line.find(str1, 0);
+				line.erase(pos,str1.length());
+				line.insert(pos,str2);
+			}
+		}
+		
+	}
+	line.insert(line.length(),"\n");
+
+	return (line);
+}
+
+void write_fich(std::string line, FILE * file)
+{
+	
+	if (file)
+		fwrite(line.data(),sizeof(char),line.size(),file);
+}
+
+
 int main(int argc, char *argv[])
 {
-
-
+	FILE * file = fopen(REPLACE_PATH,"wa");
 	if (argc != 4)
 		std::cout << "Wrong arguments" << std::endl;
 	else
@@ -17,25 +56,11 @@ int main(int argc, char *argv[])
 		{
 			while (fd.good()) {
 				std::getline(fd, line);
-				pos = line.find(target, 0);
-				if (pos != std::string::npos) {
-					if (line.find(target, pos + target.size()) != std::string::npos)
-						std::cout << "more than one coincidence ";
-					/*else
-					{
-						replaceSingleCoincidence(target, line, argv[3]);
-					}*/
-					//std::cout << "Coincidence on position " << line.find(target, 0) << " And ends on position "
-							//  << line.find(target) + target.size() << std::endl;
-				}
-				else
-					std::cout << "No coincidence in this line" << std::endl;
+				line = replaceSingleCoincidence(line,argv[2],argv[3]);
+				write_fich(line, file);
 			}
 		}
+		
 	}
 	return (0);
-}
-
-void replaceSingleCoincidence(std::string target, std::string line, std::string obj) {
-	//Function to replace single coincidence
 }

095675b2fcc7fced26e5e6ca6683988b642c6623
ordorika-benat <bgarrido42@up2u.localhost>
2022-06-07 19:18:27 +0000

    Ex04 sepepe

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <fstream>
-int main(int argc, __unused char *argv[])
+int main(int argc, char *argv[])
 {
-	std::fstream fs;
 
 
 	if (argc != 4)
 		std::cout << "Wrong arguments" << std::endl;
 	else
 	{
-		fs.open(argv[1], std::fstream::out);
-
-		fs << "lorem ipsum";
-		fs.close();
+		std::fstream fd (argv[1]);
+		std::string line;
+		if (fd.is_open())
+		{
+			while (fd.good()) {
+				std::getline(fd, line);
+				//fd >> line;
+				std::cout << line << "\n";
+			}
+		}
 	}
 	return (0);
 }

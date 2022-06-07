2c78166fd6b5122bdf82156bbeefb743614661c6
ordorika-benat <bgarrido42@up2u.localhost>
2022-06-07 20:38:51 +0000

    Ex04 sepepe

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 int main(int argc, char *argv[])
 {
 
@@ -10,14 +11,31 @@ int main(int argc, char *argv[])
 	{
 		std::fstream fd (argv[1]);
 		std::string line;
+		std::string target = argv[2];
+		std::size_t pos;
 		if (fd.is_open())
 		{
 			while (fd.good()) {
 				std::getline(fd, line);
-				//fd >> line;
-				std::cout << line << "\n";
+				pos = line.find(target, 0);
+				if (pos != std::string::npos) {
+					if (line.find(target, pos + target.size()) != std::string::npos)
+						std::cout << "more than one coincidence ";
+					/*else
+					{
+						replaceSingleCoincidence(target, line, argv[3]);
+					}*/
+					//std::cout << "Coincidence on position " << line.find(target, 0) << " And ends on position "
+							//  << line.find(target) + target.size() << std::endl;
+				}
+				else
+					std::cout << "No coincidence in this line" << std::endl;
 			}
 		}
 	}
 	return (0);
+}
+
+void replaceSingleCoincidence(std::string target, std::string line, std::string obj) {
+	//Function to replace single coincidence
 }

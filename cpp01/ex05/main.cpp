51baa6aaaaf7ab3cbf6449ae2cb2d49a6ed8bbf6
ordorika-benat <bgarrido42@up2u.localhost>
2022-06-09 17:24:41 +0000

    Ex05 La gorda de Karen

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,3 +1,15 @@
 //
 // Created by bgarrido42 on 6/8/22.
 //
+#include "Karen.hpp"
+#include <iostream>
+
+int main(){
+	Karen karen;
+
+	karen.complain("DEBUG");
+	karen.complain("INFO");
+	karen.complain("WARNING");
+	karen.complain("ERROR");
+	return (0);
+}

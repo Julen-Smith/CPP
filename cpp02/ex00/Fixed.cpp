641f1c08b294575204b7320eb04039c4af1b6667
ordorika-benat <bgarrido42@up2u.localhost>
2022-06-14 18:49:02 +0000

    Cpp02 ex00 and ex01 init

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -7,33 +7,43 @@
 //Constructor
 Fixed::Fixed()
 {
+	std::cout << "Default constructor" << std::endl;
 	this->value = 0;
 }
 
 //Copy constructor
 Fixed::Fixed(Fixed const &other)
 {
+	std::cout << "Copy constructor" << std::endl;
 	this->value = other.getRawBits();
 }
 
 //Destructor
-Fixed::~Fixed(){}
+Fixed::~Fixed(){
+	std::cout << "Destructor" << std::endl;
+}
 
 //Assignation operator overload
 //Basically asigning to the Fixed number we are calling the value of other (Copy it)
+//Object B = Object A
 Fixed &Fixed::operator=(Fixed const &other)
 {
-	this->value = other.getRawBits();
+	std::cout << "Operator" << std::endl;
+
+	if (this != &other)
+		this->value = other.getRawBits();
 	return (*this);
 }
 
 //getter
 int Fixed::getRawBits(void) const
 {
+	std::cout << "This is the value" << std::endl;
 	return (this->value);
 }
 //setter
 void Fixed::setRawBits(int const raw)
 {
+	std::cout << "Setter" << std::endl;
 	this->value = raw;
 }

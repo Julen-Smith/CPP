3b14559c11f113a2aaa361ac6854618a30b6e158
bgarrido42 <benat.garridode@gmail.com>
2022-06-16 16:16:39 +0000

    Cpp02 ex00 and ex01 init

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -15,14 +15,23 @@ Fixed::Fixed(Fixed const &other)
 {
 	this->value = other.getRawBits();
 }
-
-//parametric constructor with int
-
+/**
+ * parametric constructor with int
+ *
+ * In order to be able to safe correctly the value we must leftshift it this way
+ * https://embeddedartistry.com/blog/2018/07/12/simple-fixed-point-conversion-in-c/
+*/
 Fixed::Fixed(const int value) : value(value << Fixed::bits) {
 	return;
 }
 
-//parametric constructor with float
+/**
+ * parametric constructor with float
+ *
+ * In order to be able to save correctly the value we must left shift it
+ * https://embeddedartistry.com/blog/2018/07/12/simple-fixed-point-conversion-in-c/
+ * */
+
 Fixed::Fixed(float const value) : value(roundf(value * (1 << Fixed::bits))){}
 
 //Destructor
@@ -56,6 +65,8 @@ void Fixed::setRawBits(int const raw)
 /**
  * This formula is used to translate from binary to integer N = N/(2^x)
  * where N is the value of Fixed and x is the value of the bits shift
+ * https://embeddedartistry.com/blog/2018/07/12/simple-fixed-point-conversion-in-c/
+ * https://www.geeksforgeeks.org/left-shift-right-shift-operators-c-cpp/
  * */
 int Fixed::toInt() const {
 	return (this->value >> Fixed::bits);
@@ -65,6 +76,8 @@ int Fixed::toInt() const {
  * First we must get the left shift value of 1 in the bits asigned to fixed
  * The formula of tit is N = N * (2^x)
  * Where N is the value we want to shift and x is the number <<of the bits to shift
+ * https://embeddedartistry.com/blog/2018/07/12/simple-fixed-point-conversion-in-c/
+ * https://www.geeksforgeeks.org/left-shift-right-shift-operators-c-cpp/
  * */
 float Fixed::toFloat() const {
 	return((float) this->value / (float ) (1 << Fixed::bits));
